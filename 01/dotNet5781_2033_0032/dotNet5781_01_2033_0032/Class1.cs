2d93021b4b7b6bf5e1dad6691689655e5a0426a3
achiya danziger <achiya.danziger@gmail.com>
2020-10-22 17:43:33 +0300

    started writing class Bus

---
+++ 01/dotNet5781_2033_0032/dotNet5781_01_2033_0032/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusClass
+{
+    class Bus
+    {
+        private int licensePlateNumber;
+        private DateTime inceptionDate;
+        private float fuel;
+        private float mileage;
+        private float maileageInLastRepair;
+        private DateTime timeOfRefuel;
+        /*
+            * Constructor.
+
+       */
+        public Bus(int _licensePlateNumber, DateTime _date)
+        {
+        
+        
+        }
+
+
+    }
+}
--- 01/dotNet5781_2033_0032/dotNet5781_01_2033_0032/Program.cs
+++ 01/dotNet5781_2033_0032/dotNet5781_01_2033_0032/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusClass;
 
 namespace dotNet5781_01_2033_0032
 {
