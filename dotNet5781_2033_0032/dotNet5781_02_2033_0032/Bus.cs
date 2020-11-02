2f715b8dd1462eb80c312d5f31a280c4e3987c08
NoamSch <noams1125@gmail.com>
2020-11-02 17:50:22 +0200

    added some functions.

--- dotNet5781_2033_0032/dotNet5781_02_2033_0032/Bus.cs
+++ dotNet5781_2033_0032/dotNet5781_02_2033_0032/Bus.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,7 +20,6 @@ namespace dotNet5781_02_2033_0032
         protected float longitude;
         protected string stationAddress;
         #endregion
-
         public override string ToString()
         {
             return (string)("Bus Station Code: " + busStationKey + " "
@@ -31,9 +31,25 @@ namespace dotNet5781_02_2033_0032
     {
         private float distFromLastStation;
         private float timeSinceLastStation;
+
+        public float getDistFromLastStation
+        {
+            get
+            {
+                return distFromLastStation;
+            }
+        }
+
+        public float getTimeSinceLastStation
+        {
+            get
+            {
+                return getTimeSinceLastStation;
+            }
+        }
     }
 
-    class BusLine
+    class BusLine : IComparable
     {
         #region variables
         private int busLine;
@@ -92,7 +108,67 @@ namespace dotNet5781_02_2033_0032
         {
             return stations.Contains(_station);
         }
+
+        public float distance(BusStationLine station1, BusStationLine station2)
+        {
+            int index1 = stations.FindIndex(x => station1 == x);
+            int index2 = stations.FindIndex(x => station2 == x);
+            float distance = 0;
+            if (index1 >= index2)
+            {
+                index1 = index1 ^ index2;
+                index2 = index1 ^ index2;
+                index1 = index1 ^ index2;
+            }
+
+            for (int i = index1 + 1; i <= index2; i++)
+                distance += stations[i].getDistFromLastStation;
+
+            return distance;
+
+        }
+
+        public float time(BusStationLine station1, BusStationLine station2)
+        {
+            int index1 = stations.FindIndex(x => station1 == x);
+            int index2 = stations.FindIndex(x => station2 == x);
+            float time = 0;
+            if (index1 >= index2)
+            {
+                index1 = index1 ^ index2;
+                index2 = index1 ^ index2;
+                index1 = index1 ^ index2;
+            }
+
+            for (int i = index1 + 1; i <= index2; i++)
+                time += stations[i].getTimeSinceLastStation;
+
+            return time;
+
+        }
+
+        public BusLine subLine(BusStationLine station1, BusStationLine station2)
+        {
+            BusLine temp = new BusLine();
+            int index1 = stations.FindIndex(x => station1 == x);
+            int index2 = stations.FindIndex(x => station2 == x);
+            int length = Math.Abs(index1 - index2) + 1;
+
+            temp.stations = stations.GetRange(Math.Min(index1, index2), length);
+            temp.area = area;
+            temp.busLine = busLine;
+            temp.firstStation = temp.stations[0];
+            temp.lastStation = temp.stations[length - 1];
+
+            return temp;
+        }
         #endregion
+
+        BusLine IComparable.CompareTo(object obj)
+        {
+            
+            
+        }
 
     }
 }
