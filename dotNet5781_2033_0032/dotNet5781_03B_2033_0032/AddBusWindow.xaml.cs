9f8100d73252c964c2da8a72f89524e264d9e902
NoamSch <noams1125@gmail.com>
2020-11-24 18:59:53 +0200

    Added a new window + support for buttons

---
+++ dotNet5781_2033_0032/dotNet5781_03B_2033_0032/AddBusWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace dotNet5781_03B_2033_0032
+{
+    /// <summary>
+    /// Interaction logic for AddBusWindow.xaml
+    /// </summary>
+    public partial class AddBusWindow : Window
+    {
+        MainWindow win;
+        public AddBusWindow(MainWindow _win)
+        {
+            win = _win;
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.buses.Add(new Bus(int.Parse(license.Text), DateTime.Parse(date.Text)));
+            Hide();
+            win.addBus(MainWindow.buses.Count - 1);
+        }
+
+        private void mouseEnter(object sender, EventArgs e)
+        {
+           if ((sender as TextBox).Text == (sender as TextBox).Tag.ToString())
+                (sender as TextBox).Text  = string.Empty;
+        }
+
+    }
+}
--- dotNet5781_2033_0032/dotNet5781_03B_2033_0032/MainWindow.xaml.cs
+++ dotNet5781_2033_0032/dotNet5781_03B_2033_0032/MainWindow.xaml.cs
@@ -20,48 +20,43 @@ namespace dotNet5781_03B_2033_0032
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<Bus> buses;
+        public static List<Bus> buses;
 
         public MainWindow()
         {
             InitializeComponent();
             initilize(ref buses);
 
-
             for (int i = 0; i < buses.Count; i++)
-            {
-                var text = new TextBlock();
-                text.Text = buses[i].licensePlate.ToString();
-                text.FontSize = 15;
-                var useButton = new Button();
-                useButton.Content = "Use";
-                var fuelButton = new Button();
-                fuelButton.Content = "Refuel";
-                var fixButton = new Button();
-                fixButton.Content = "Fix";
+                addBus(i);
+        }
 
-
-                Grid.SetRow(text, i); Grid.SetRow(useButton, i); Grid.SetRow(fuelButton, i); Grid.SetRow(fixButton, i);
-                Grid.SetColumn(text, 0); Grid.SetColumn(useButton, 1); Grid.SetColumn(fuelButton, 2); Grid.SetColumn(fixButton, 3);
-
-                GridData.Children.Add(text); GridData.Children.Add(useButton); GridData.Children.Add(fuelButton); GridData.Children.Add(fixButton);
-
-                GridData.RowDefinitions.Add(new RowDefinition());
-
-
-
-
-
-
-
-            }
-
+        public void addBus(int index)
+        {
+            GridData.RowDefinitions.Add(new RowDefinition());
+            var text = new TextBlock();
+            text.Text = buses[index].licensePlate.ToString();
+            text.FontSize = 15;
+            text.TextAlignment = TextAlignment.Center;
+            var useButton = new Button();
+            useButton.Content = "Use";
+            useButton.Click += new RoutedEventHandler(useClick);
+            var fuelButton = new Button();
+            fuelButton.Content = "Refuel";
+            var fixButton = new Button();
+            fixButton.Content = "Fix";
+            Grid.SetRow(text, index); Grid.SetRow(useButton, index); Grid.SetRow(fuelButton, index); Grid.SetRow(fixButton, index);
+            Grid.SetColumn(text, 0); Grid.SetColumn(useButton, 1); Grid.SetColumn(fuelButton, 2); Grid.SetColumn(fixButton, 3);
+            GridData.Children.Add(text); GridData.Children.Add(useButton); GridData.Children.Add(fuelButton); GridData.Children.Add(fixButton);
 
         }
 
+        public void useClick(object sender, RoutedEventArgs e)
+        {
+            int i = Grid.GetRow((sender as Button));
+            buses[i].rideKM(15);
 
-
-
+        }
 
 
         static void initilize(ref List<Bus> buses)
@@ -70,7 +65,7 @@ namespace dotNet5781_03B_2033_0032
             Random rnd = new Random(DateTime.Now.Millisecond);
             int plateNumber, range, mileage, mileageInLastTreat, fuel;
             DateTime date;
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 15; i++)
             {
                 DateTime start = new DateTime(2016, 1, 1);
                 range = (DateTime.Today - start).Days;
@@ -95,6 +90,12 @@ namespace dotNet5781_03B_2033_0032
             buses.Add(new Bus(2222222, new DateTime(2012, 2, 2), new DateTime(2020, 2, 2), 19500, 0, 1000)); // Need a treatment (20K km has passed)
             buses.Add(new Bus(3333333, new DateTime(2013, 3, 3), new DateTime(2020, 3, 3), 1000, 900, 20)); // Need a refuel (almost ran out of fuel)
 
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var v = new AddBusWindow(this);
+            v.Show();
         }
     }
 }
