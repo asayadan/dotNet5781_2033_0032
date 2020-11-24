df927b6f26856f84177f092a8c328b4e51248c71
NoamSch <noams1125@gmail.com>
2020-11-24 15:12:58 +0200

    Started 3B

---
+++ dotNet5781_2033_0032/dotNet5781_03B_2033_0032/Bus.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+enum Status
+{
+    ready, working, refueling, fixing
+};
+
+namespace dotNet5781_03B_2033_0032
+{
+    class Bus
+    {
+        #region variables
+        private int licensePlateNumber;
+        private DateTime registreationDate;
+        private float fuel;
+        private float mileage;
+        private float maileageInLastTreatment;
+        private DateTime timeOfLastTreatment;
+
+        public static int FULL_GAS_TANK = 1200;//const the size of full gas tank
+        #endregion
+
+
+        #region ctor
+        /*
+            * Constructor.
+            * checkes  if the input is valid 
+            * and Initializes the date and the license plate number
+
+       */
+        public Bus(int _licensePlateNumber, DateTime _date)
+        {   
+                licensePlateNumber = _licensePlateNumber;
+                registreationDate = _date;
+                timeOfLastTreatment = _date;
+                mileage = 0;
+                maileageInLastTreatment = 0;
+                fuel = FULL_GAS_TANK;//we assume that every bus starts with full gas tank
+            
+        }
+
+        public Bus(int _licensePlateNumber, DateTime _date, DateTime _lastTreatment, float _mileage, float _maileageInLastTreatment, float _fuel)
+        {
+            licensePlateNumber = _licensePlateNumber;
+            registreationDate = _date;
+            timeOfLastTreatment = _lastTreatment;
+            mileage = _mileage;
+            maileageInLastTreatment = _maileageInLastTreatment;
+            fuel = _fuel;
+
+        }
+        #endregion
+
+
+        #region properties
+        public int licensePlate//returns the license plate number
+        {get { return licensePlateNumber; }}
+
+        public DateTime LastTreatment//returns the license plate number
+        { get { return timeOfLastTreatment; } 
+          set { timeOfLastTreatment = value; }
+        }
+        public float _mileage//returns the number of km the bus drove untile now
+        { get { return mileage; }}
+
+        public float _fuel//returns the number of km the bus can ride without Refueling
+        { get { return fuel; } }
+
+        public float mileageSinceTreatment//returns the mileage since last treatment
+        { get { return mileage - maileageInLastTreatment; } }
+
+        //add ride  getting the number of km to ride
+        public void rideKM(int value)
+        {
+            if (mileageSinceTreatment > 20000 || _fuel < value)//checking if the bus can do this ride
+                Console.WriteLine("This bus can't be used!");
+            else {
+                mileage += value;//adding the number of km who were being traveled to the mileage
+                fuel -= value;//Subtracting  the number of km who were being traveled from the amount we can ride without Refueling 
+                Console.WriteLine("Ride finished succesfuly!");
+            }
+        }
+
+        public void refuel()//we set the number of km the bus can ride without Refueling to the maximum value
+        {
+            fuel =FULL_GAS_TANK;
+        }
+
+
+
+        public void treatment(DateTime _date)
+        {
+            maileageInLastTreatment = mileage;
+            timeOfLastTreatment = _date;
+        }
+
+
+        //we print the license plate number on the right format
+        public void Print_licensePlateNumber()
+        {
+            if (registreationDate.Year>=2018)//the number has 8 digits
+            {
+                Console.Write((licensePlateNumber/100000).ToString()+"-");//first three numbers and hyphen
+                Console.Write(((licensePlateNumber / 1000)%100).ToString() + "-");// the two middle numbers numbers and hyphen
+                Console.Write((licensePlateNumber % 1000).ToString());//the last three numbers
+
+            }
+            else  //the number has 7 digits (the license plate number is valid)
+            {
+                Console.Write((licensePlateNumber / 100000).ToString() + "-");//first two numbers and hyphen
+                Console.Write(((licensePlateNumber / 100) % 1000).ToString() + "-");// the three middle numbers numbers and hyphen
+                Console.Write((licensePlateNumber % 100).ToString());//the last two numbers
+
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ dotNet5781_2033_0032/dotNet5781_03B_2033_0032/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dotNet5781_03B_2033_0032
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Bus> buses;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            initilize(ref buses);
+
+
+            for (int i = 0; i < buses.Count; i++)
+            {
+                var text = new TextBlock();
+                text.Text = buses[i].licensePlate.ToString();
+                text.FontSize = 15;
+                var useButton = new Button();
+                useButton.Content = "Use";
+                var fuelButton = new Button();
+                fuelButton.Content = "Refuel";
+                var fixButton = new Button();
+                fixButton.Content = "Fix";
+
+
+                Grid.SetRow(text, i); Grid.SetRow(useButton, i); Grid.SetRow(fuelButton, i); Grid.SetRow(fixButton, i);
+                Grid.SetColumn(text, 0); Grid.SetColumn(useButton, 1); Grid.SetColumn(fuelButton, 2); Grid.SetColumn(fixButton, 3);
+
+                GridData.Children.Add(text); GridData.Children.Add(useButton); GridData.Children.Add(fuelButton); GridData.Children.Add(fixButton);
+
+                GridData.RowDefinitions.Add(new RowDefinition());
+
+
+
+
+
+
+
+            }
+
+
+        }
+
+
+
+
+
+
+        static void initilize(ref List<Bus> buses)
+        {
+            buses = new List<Bus>();
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            int plateNumber, range, mileage, mileageInLastTreat, fuel;
+            DateTime date;
+            for (int i = 0; i < 10; i++)
+            {
+                DateTime start = new DateTime(2016, 1, 1);
+                range = (DateTime.Today - start).Days;
+                date = start.AddDays(rnd.Next(range));
+
+                if (date.Year < 2018)
+                    plateNumber = rnd.Next(1000000, 9999999);
+                else plateNumber = rnd.Next(10000000, 99999999);
+
+                //start = date;
+                //range = (DateTime.Today - date).Days;
+                //lastTreatment = start.AddDays(rnd.Next(range));
+
+                mileage = rnd.Next(5000);
+                mileageInLastTreat = rnd.Next(mileage);
+                fuel = rnd.Next(1200);
+
+                buses.Add(new Bus(plateNumber, date, date, mileage, mileageInLastTreat, fuel));
+            }
+
+            buses.Add(new Bus(1111111, new DateTime(2011, 1, 1), new DateTime(2018, 1, 1), 30000, 29000, 1000)); // Need a treatment (a year has passed)
+            buses.Add(new Bus(2222222, new DateTime(2012, 2, 2), new DateTime(2020, 2, 2), 19500, 0, 1000)); // Need a treatment (20K km has passed)
+            buses.Add(new Bus(3333333, new DateTime(2013, 3, 3), new DateTime(2020, 3, 3), 1000, 900, 20)); // Need a refuel (almost ran out of fuel)
+
+        }
+    }
+}
