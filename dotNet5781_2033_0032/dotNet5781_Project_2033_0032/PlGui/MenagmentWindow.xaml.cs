9ba88cc00c5460f384c7a06cb4ade760e866f5a0
achiya danziger <achiya.danziger@gmail.com>
2021-01-13 10:42:16 +0200

    minor changes

--- dotNet5781_2033_0032/dotNet5781_Project_2033_0032/PlGui/MenagmentWindow.xaml.cs
+++ dotNet5781_2033_0032/dotNet5781_Project_2033_0032/PlGui/MenagmentWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,7 +23,7 @@ namespace PlGui
         BackgroundWorker lineWorker = new BackgroundWorker();
         ObservableCollection<BO.Line> lineCollection= new ObservableCollection<BO.Line>();
         BackgroundWorker stationsInLineWorker = new BackgroundWorker();
-        ObservableCollection<BO.Station> stationsInLine;
+        ObservableCollection<BO.Station> stationsInLineCollection=new ObservableCollection<BO.Station>();
 
         BO.Line curLine;
         BO.Bus curBus;
@@ -33,27 +34,47 @@ namespace PlGui
             InitializeComponent();
             SetBusTab();
             SetLinesTab();
-            
         }
 
         #region setters
         void SetLinesTab()
         {
-            gridLine.DataContext = curLine;
-            StationsInLineDataGrid.DataContext = stationsInLine;
-            cb_lines.DataContext = lineCollection;
+           // lineCollection.CollectionChanged += LineViewChanged;
             cb_lines.DisplayMemberPath = "Code";//show only specific Property of object
             cb_lines.SelectedValuePath = "Id";//selection return only specific Property of object
             cb_lines.SelectedIndex = 0; //index of the object to be selected
+            gridLine.DataContext = curLine;
+            StationsInLineDataGrid.DataContext = stationsInLineCollection;
+            cb_lines.DataContext = lineCollection;
             areaComboBox.ItemsSource = Enum.GetValues(typeof(BO.Areas));
             lineWorker.DoWork+=SetAllLines;
             lineWorker.RunWorkerAsync();
-
         }
 
         void SetAllLines(object sender, DoWorkEventArgs e)
         {
-            lineCollection = new ObservableCollection<BO.Line>(bl.GetAllLines());
+
+            var help = bl.GetAllLines();
+            foreach (var item in help)
+            {
+                lineCollection.Add(item);
+            }
+        }
+        void setAllStations(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                var a = bl.GetLineStationsInLine((int)e.Argument);
+                stationsInLineCollection.Clear();
+                foreach (var b in a)
+                {
+                    stationsInLineCollection.Add(bl.GetStation(b.StationId));
+                }
+            }
+            catch (BO.InvalidStationIDException)
+            {
+
+            }
         }
 
         void SetBusTab()
@@ -68,22 +89,6 @@ namespace PlGui
         {
             cbBuses.DataContext = bl.GetAllBuses();
         }
-
-        void setAllStations(object sender, DoWorkEventArgs e)
-        {
-            var a = bl.GetLineStationsInLine((int)cb_lines.SelectedValue);
-            stationsInLine.Clear();
-            foreach (var b in a)
-            {
-                stationsInLine.Add(bl.GetStation(b.StationId));
-            }
-        }
-        #endregion
-        #region backgroundWorker
-        private void Worker(object sender, DoWorkEventArgs e)
-        {
-
-        }
         #endregion
         #region Line functions
         private void cb_lines_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -93,8 +98,8 @@ namespace PlGui
             if (cb_lines.SelectedValue != null)
             {
                 stationsInLineWorker.DoWork += setAllStations;
-                stationsInLineWorker.RunWorkerAsync();
-                stationsInLineWorker.DoWork -= setAllStations;
+                stationsInLineWorker.RunWorkerAsync((int)cb_lines.SelectedValue);
+                //stationsInLineWorker.DoWork -= setAllStations;
             }
         }
         private void btRemoveStation_Click(object sender, RoutedEventArgs e)
