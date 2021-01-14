65a2a1a114273539f7e69a56ccf8b8fca14215ed
achiya danziger <achiya.danziger@gmail.com>
2021-01-14 19:53:14 +0200

    finished signUp

--- dotNet5781_2033_0032/dotNet5781_Project_2033_0032/PlGui/SignUp.xaml.cs
+++ dotNet5781_2033_0032/dotNet5781_Project_2033_0032/PlGui/SignUp.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BLAPI;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +21,51 @@ namespace PlGui
     /// </summary>
     public partial class SignUp : Window
     {
-        public SignUp()
+        IBL bl;
+        BackgroundWorker signWorker=new BackgroundWorker() ;
+        public SignUp(IBL _bl)
         {
+            bl = _bl;
+            signWorker.DoWork += SignUpFunction;
             InitializeComponent();
+        }
+
+        void SignUpFunction(object sender, DoWorkEventArgs e)
+        {
+            PlGui.User user = (PlGui.User)e.Argument;
+            try
+            {
+                bl.AddUser(user.username, user.password,user.varifyPassword);
+                this.Dispatcher.Invoke((Action)(() =>
+                {
+                    this.Close();
+                }));
+            }
+            catch (BO.BadUsernameOrPasswordException ex)
+            {
+                if (ex.InnerException!=null)
+                {
+                    this.Dispatcher.Invoke((Action)(() =>
+                    {
+                        tbl_warnings.Text = "that username is taker try another username";
+                    }));
+                }
+                else
+                {
+                    this.Dispatcher.Invoke((Action)(() =>
+                    {
+                        tbl_warnings.Text = "the password doesn't match";
+                    }));
+                }
+            }
+
+        }
+
+
+        private void bn_signUp_Click(object sender, RoutedEventArgs e)
+        {
+
+            signWorker.RunWorkerAsync(new PlGui.User { username = tb_username.Text, password = pb_password.Password,varifyPassword=pb_passwordValidation.Password });
         }
     }
 }
